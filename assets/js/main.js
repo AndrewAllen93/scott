4082e62433b4965e03e364496f5134dd6dc7d167
AndrewAllen93 <allenkandrew@gmail.com>
2024-04-19 13:22:44 -0500

    changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,44 +1,61 @@
-/*=============== SHOW MENU ===============*/
-const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close')
+document.addEventListener("DOMContentLoaded", () => {
+  /*=============== SHOW MENU ===============*/
+  const navMenu = document.getElementById("nav-menu"),
+    navToggle = document.getElementById("nav-toggle"),
+    navClose = document.getElementById("nav-close");
 
-/* Menu show */
-navToggle.addEventListener('click', () =>{
-   navMenu.classList.add('show-menu')
-})
+  /* Menu show */
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
 
-/* Menu hidden */
-navClose.addEventListener('click', () =>{
-   navMenu.classList.remove('show-menu')
-})
+  /* Menu hidden */
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
 
-/*=============== SEARCH ===============*/
-const search = document.getElementById('search'),
-      searchBtn = document.getElementById('search-btn'),
-      searchClose = document.getElementById('search-close')
+  /*=============== SEARCH ===============*/
+  const search = document.getElementById("search"),
+    searchBtn = document.getElementById("search-btn"),
+    searchClose = document.getElementById("search-close");
 
-/* Search show */
-searchBtn.addEventListener('click', () =>{
-   search.classList.add('show-search')
-})
+  /* Search show */
+  searchBtn.addEventListener("click", () => {
+    search.classList.add("show-search");
+  });
 
-/* Search hidden */
-searchClose.addEventListener('click', () =>{
-   search.classList.remove('show-search')
-})
+  /* Search hidden */
+  searchClose.addEventListener("click", () => {
+    search.classList.remove("show-search");
+  });
 
-/*=============== LOGIN ===============*/
-const login = document.getElementById('login'),
-      loginBtn = document.getElementById('login-btn'),
-      loginClose = document.getElementById('login-close')
+  /*=============== LOGIN ===============*/
+  const login = document.getElementById("login"),
+    loginBtn = document.getElementById("login-btn"),
+    loginClose = document.getElementById("login-close");
 
-/* Login show */
-loginBtn.addEventListener('click', () =>{
-   login.classList.add('show-login')
-})
+  /* Login show */
+  loginBtn.addEventListener("click", () => {
+    login.classList.add("show-login");
+  });
 
-/* Login hidden */
-loginClose.addEventListener('click', () =>{
-   login.classList.remove('show-login')
-})
+  /* Login hidden */
+  loginClose.addEventListener("click", () => {
+    login.classList.remove("show-login");
+  });
+
+  // Get the navbar element
+  const navbar = document.getElementById("navbar");
+
+  // Listen for scroll events on the window
+  window.addEventListener("scroll", () => {
+    // Check if the scroll position is greater than 0
+    if (window.scrollY > 0) {
+      // Add the 'navbar-hidden' class if the scroll position is greater than 0
+      navbar.classList.add("navbar-hidden");
+    } else {
+      // Remove the 'navbar-hidden' class if the scroll position is 0
+      navbar.classList.remove("navbar-hidden");
+    }
+  });
+});
