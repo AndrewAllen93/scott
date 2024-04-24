71702b13d0a32656145ce7a337902a16f949cc4b
AndrewAllen93 <allenkandrew@gmail.com>
2024-04-23 21:21:45 -0500

    changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,68 +17,137 @@ if(navClose){
    })
 }
 
-const slides = document.querySelectorAll('.slide');
-const next = document.querySelector('#next');
-const prev = document.querySelector('#prev');
-const auto = true;
-const intervalTime = 5000;
-let slideInterval;
-
-// Next Slide Function
-const nextSlide = () => {
-    // Get the current class
-    const current = document.querySelector('.current');
-    // Remove the current class
-    current.classList.remove('current');
-    if(current.nextElementSibling){
-    // Add the current class to the next slide
-        current.nextElementSibling.classList.add('current');
-    }else{
-        // Add the current class to the first when done from all
-        slides[0].classList.add('current');
-    }
-    setTimeout(() => current.classList.remove('current'));
-}
-
-// Previous Slide Function
-const prevSlide = () => {
-    // Get the current class
-    const current = document.querySelector('.current');
-    // Remove the current class
-    current.classList.remove('current');
-    if(current.previousElementSibling){
-    // Add the current class to the previous slide
-        current.previousElementSibling.classList.add('current');
-    }else{
-        // Add the current class to the last when done from all
-        slides[slides.length - 1].classList.add('current');
-    }
-    setTimeout(() => current.classList.remove('current'));
-}
-
-// Button Events
-next.addEventListener('click', e => {
-    nextSlide();
-    if(auto){
-        // Clear interval time 
-        clearInterval(slideInterval);
-        // Run next slide at interval time
-        slideInterval = setInterval(nextSlide, intervalTime);
-    }
-})
-
-prev.addEventListener('click', e => {
-    prevSlide();
-    if(auto){
-        // Clear interval time 
-        clearInterval(slideInterval);
-        // Run next slide at interval time
-        slideInterval = setInterval(nextSlide, intervalTime);
-    }
-})
-
-// Auto Slide
-if(auto){
-    // Run next slide at interval time
-    slideInterval = setInterval(nextSlide, intervalTime);
-}
+var SliceSlider = {
+    
+   /**
+    * Settings Object
+    */
+   settings: {
+     delta:              0,
+     currentSlideIndex:  0,
+     scrollThreshold:    40,
+     slides:             document.querySelectorAll('.slide'),
+     numSlides:          document.querySelectorAll('.slide').length,
+     navPrev:            document.querySelectorAll('.js-prev'),
+     navNext:            document.querySelectorAll('.js-next'),
+   },
+   
+   /**
+    * Init
+    */
+   init: function() {
+     this.bindEvents();
+   },
+   
+   /**
+    * Bind our click, scroll, key events
+    */
+   bindEvents: function(){
+     var self = this;
+     // Scrollwheel & trackpad
+     this.settings.slides.forEach(function(slide) {
+       slide.addEventListener('wheel', function(e) {
+         self.handleScroll(e);
+       });
+     });
+     // On click prev
+     this.settings.navPrev.forEach(function(nav) {
+       nav.addEventListener('click', function() {
+         self.prevSlide();
+       });
+     });
+     // On click next
+     this.settings.navNext.forEach(function(nav) {
+       nav.addEventListener('click', function() {
+         self.nextSlide();
+       });
+     });
+     // On Arrow keys
+     document.addEventListener('keyup', function(e) {
+       // Left or back arrows
+       if ((e.which === 37) ||  (e.which === 38)){
+         self.prevSlide();
+       }
+       // Down or right
+       if ((e.which === 39) ||  (e.which === 40)) {
+         self.nextSlide();
+       }
+     });
+   },
+   
+   /** 
+    * Intercept scroll direction
+    */
+   handleScroll: function(e){
+     var self = SliceSlider;
+     // Scrolling up
+     if (e.deltaY < 0) { 
+       self.settings.delta--;
+       if ( Math.abs(self.settings.delta) >= self.settings.scrollThreshold) {
+         self.prevSlide();
+       }
+     }
+     // Scrolling Down
+     else {
+       self.settings.delta++;
+       if (self.settings.delta >= self.settings.scrollThreshold) {
+         self.nextSlide();
+       }
+     }
+     // Prevent page from scrolling
+     e.preventDefault();
+   },
+ 
+   /**
+    * Show Slide
+    */
+   showSlide: function(){ 
+     var self = this;
+     // Reset
+     this.settings.delta = 0;
+     // Bail if we're already sliding
+     if (document.body.classList.contains('is-sliding')){
+       return;
+     }
+     // Loop through our slides
+     this.settings.slides.forEach(function(slide, i) {
+       // Toggle the is-active class to show slide
+       slide.classList.toggle('is-active', (i === self.settings.currentSlideIndex)); 
+       slide.classList.toggle('is-prev', (i === self.settings.currentSlideIndex - 1)); 
+       slide.classList.toggle('is-next', (i === self.settings.currentSlideIndex + 1)); 
+       // Add and remove is-sliding class
+       document.body.classList.add('is-sliding');
+       setTimeout(function(){
+         document.body.classList.remove('is-sliding');
+       }, 1000);
+     });
+   },
+ 
+   /**
+    * Previous Slide
+    */
+   prevSlide: function(){
+     var self = this;
+     // If on first slide, loop to last
+     if (this.settings.currentSlideIndex <= 0) {
+       this.settings.currentSlideIndex = this.settings.numSlides;
+     }
+     this.settings.currentSlideIndex--;
+     this.showSlide();
+   },
+ 
+   /**
+    * Next Slide
+    */
+   nextSlide: function(){
+     var self = this;
+     this.settings.currentSlideIndex++;
+     // If on last slide, loop to first
+     if (this.settings.currentSlideIndex >= this.settings.numSlides) { 
+       this.settings.currentSlideIndex = 0;
+     }
+     this.showSlide();
+   },
+ };
+ SliceSlider.init();
+ 
