456c48f4719b4dfb69e55d75854dc5c481856286
AndrewAllen93 <allenkandrew@gmail.com>
2024-05-06 10:48:43 -0500

    changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,17 +17,68 @@ if(navClose){
    })
 }
 
-var swiper = new Swiper(".mySwiper", {
-   slidesPerView: 1,
-   loop: true,
-   pagination: {
-     el: ".swiper-pagination",
-     clickable: true,
-   },
-   navigation: {
-     nextEl: ".swiper-button-next",
-     prevEl: ".swiper-button-prev",
-   },
- });
+// buttons
+const sliderControl = document.querySelector(".slider-control");
+
+// slides informations
+const slides = document.querySelectorAll(".slide");
+const slidesLength = slides.length;
+
+// slides array
+let slidesArr = Array.from(slides);
+
+// reverse array sorting
+slidesArr = slidesArr.reverse();
+
+// slide current
+let slideCurrent = 0;
+
+sliderControl.addEventListener("click", function (e) {
+  const target = e.target.closest(".control");
+
+  if (!target) return; // If the clicked element is not a control button, exit the function
+
+  const isNext = target.classList.contains("next");
+
+  if (isNext) {
+    slideCurrent++;
+  } else {
+    slideCurrent--;
+  }
+
+  // Update classes for slides
+  slidesArr.forEach((slide, index) => {
+    if (index === slideCurrent) {
+      slide.classList.add("text-on");
+      slide.classList.remove("slide-on");
+    } else {
+      slide.classList.remove("text-on");
+      slide.classList.add("slide-on");
+    }
+  });
+
+  // Enable/disable next/prev buttons
+  const prevButton = document.querySelector(".control.prev");
+  const nextButton = document.querySelector(".control.next");
+  
+  if (slideCurrent === 0) {
+    prevButton.classList.add("disabled");
+  } else {
+    prevButton.classList.remove("disabled");
+  }
+  
+  if (slideCurrent === slidesLength - 1) {
+    nextButton.classList.add("disabled");
+  } else {
+    nextButton.classList.remove("disabled");
+  }
+});
+
+// balapaCop function call
+balapaCop("Image Slider", "#999");
+
+
+
+
 
 
